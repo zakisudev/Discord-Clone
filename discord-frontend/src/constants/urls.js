99af2cf09225679b9c44f34d1aabc6db4f520040
Zekaria Mohammed <zakisu250@gmail.com>
2024-01-21 22:59:29 +0300

    Update BASE_URL in urls.js

--- discord-frontend/src/constants/urls.js
+++ discord-frontend/src/constants/urls.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:5000/api';
+const BASE_URL = '';
 export const LOGIN_URL = `${BASE_URL}/auth/login`;
 export const REGISTER_URL = `${BASE_URL}/auth/register`;
 export const LOGOUT_URL = `${BASE_URL}/auth/logout`;
