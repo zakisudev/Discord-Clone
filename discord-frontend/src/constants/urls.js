accc4028877b20261323a24491d00e55cd6b2d74
Zekaria Mohammed <zakisu250@gmail.com>
2024-01-21 23:04:36 +0300

    Update API proxy and base URL

--- discord-frontend/src/constants/urls.js
+++ discord-frontend/src/constants/urls.js
@@ -1,4 +1,4 @@
-const BASE_URL = '';
+const BASE_URL = '/api';
 export const LOGIN_URL = `${BASE_URL}/auth/login`;
 export const REGISTER_URL = `${BASE_URL}/auth/register`;
 export const LOGOUT_URL = `${BASE_URL}/auth/logout`;
